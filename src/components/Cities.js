380aab9978a945bef859c6c717897f7bbacab0ca
Hamza4241 <hamza88851@gmail.com>
2023-07-31 14:43:12 +0500

    Added a comment

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -131,6 +131,7 @@ const Cities = () => {
               <div className='image-btns sm:basis-3/5 flex flex-row justify-between items-center'>
                 <div className='image'>
                   <img className='h-5/6' src={`https://openweathermap.org/img/wn/${city.code}@2x.png`} alt="Weather Icon" />
+                  {/* I have made weather icon visible on first load */}
                 </div>
 
                 <div className='btn'>
